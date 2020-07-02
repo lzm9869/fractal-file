3c564d54a0c45d73392b62df7c24672c0ccc56fe
Matthew Peeks <mep9218@rit.edu>
2020-07-02 12:04:21 -0400

    Reworked the graph class
    Did a complete rework of the graph class and completely removed the vertex class to try and remove the problem where deep copy would not be able to copy SC higher than 3. Still broken, will probably need to use deep copy anyway but will need to do many small copies instead of one big one somehow.

--- graphClasses.py
+++ graphClasses.py
@@ -2,85 +2,62 @@ import numpy as np
 import copy
 
 
-class Vertex:
-    # an object with a name, a list of neighbors, and a list of weights
-    # name is a string that the vertex is referred to
-    # neighbors is a list of Vertex objects that corresponds to all the vertices this vertex shares an edge with
-    # weights is a list of floats that corresponds to the weight of the edge connecting this vertex
-    #   and the neighbor at the same index
-    # position is a list of 2 values, [x-coordinate, y-coordinate]
-    # example: name = 'A'; neighbors = ('B','C'); weights = (4,10)
-    # the above means that vertex A shares an edge of weight 4 with vertex B and an edge of weight 10 with vertex C
-    def __init__(self, n):
-        self.name = n
-        self.neighbors = list()
-        self.position = None
-        self.harmonicValue = None
-
-    # if there are optimization problems, might be able to change this so that the neighbor lists
-    #   are lists of strings, not Vertex objects
-    def add_neighbor(self, v):
-        if v not in self.neighbors:
-            self.neighbors.append(v)
-
-
 class Graph:
-    # an object that is simply a list of vertices
+    # an object that is simply a dictionary of vertices
+    # the key is the name of the vertex and refers to a list
+    # index 0 is a list of strings referring to the keys of the vertex's neighbors
+    # index 1 is a numpy array of 2 values referring to the x and y coordinates of the vertex, respectively
+    # index 2 is the harmonicValue of the point
     def __init__(self):
-        self.vertices = []
+        self.vertices = {}
 
-    def add_vertex(self, vertex):
+    # noinspection PyUnusedLocal
+    def add_vertex(self, name, xyCoordinates):
         # allows you to add vertices to a graph
-        # checks that the input was a vertex and that the vertex is not already in the graph
-        if isinstance(vertex, Vertex) and vertex not in self.vertices:
-            self.vertices.append(vertex)
-            return True
-        else:
-            return False
+        # adds a new key to the vertices dictionary that refers to the list corresponding to the vertex
+        # the key is the string input for name, the position input should be a list and is transformed into an np.array
+        # and 0 is put as a placeholder harmonic value
+        self.vertices[name] = [[], np.array(xyCoordinates), 0]
 
     def add_edge(self, u, v):
         # allows you to add edges to vertices in a graph
-        # checks that both vertices (u, v) are in the graph, then makes them neighbors of each other
+        # input is 2 strings, function makes sure the strings are both keys in the vertices dictionary and then adds
+        #   each string to each other's neighbor lists
         if u in self.vertices and v in self.vertices:
-            u.add_neighbor(v)
-            v.add_neighbor(u)
-            return True
-        else:
-            return False
+            self.vertices[v][0].append(u)
+            self.vertices[u][0].append(v)
 
     def contract_graph(self, scale, fixedPoint):
         # contracts a graph by a scale towards a fixed point
-        # since v.position and fixedPoint are numpy arrays, the formula written works
+        # since v[1] (the position) and fixedPoint are numpy arrays, the formula written works
         # NOTICE: will need to change this once the code is modified for affine graphs
         for v in self.vertices:
-            v.position = scale * (v.position - fixedPoint) + fixedPoint
+            self.vertices[v][1] = scale * (self.vertices[v][1] - fixedPoint) + self.vertices[v][1]
 
     def combine_vertices(self, u, v):
-        # little sloppy but should work for our purposes
-        if v in self.vertices and u in self.vertices:
-            pointsWithVAsNeighbor = list()
-            for n in v.neighbors:
-                pointsWithVAsNeighbor.append(n)
-                u.neighbors.append(n)
-            self.vertices.remove(v)
-            for p in pointsWithVAsNeighbor:
-                p.neighbors.remove(v)
-                p.neighbors.append(u)
+        for n in self.vertices[v][0]:
+            self.vertices[u][0].append(n)
+            for neigh in self.vertices[v][0]:
+                self.vertices[neigh][0].remove(v)
+                self.vertices[neigh][0].append(u)
+        self.vertices.pop(v)
 
-    def add_graph(self, g):
+    def update_all_vertices_names(self, update):
+        self.vertices = {key+update: value for key, value in self.vertices.items()}
+
+    def add_graph(self, gr):
         # copies all the Vertex objects from a graph to another graph
-        for v in g.vertices:
-            self.add_vertex(v)
+        grCopy = copy.deepcopy(gr)
+        self.vertices.update(grCopy.vertices)
 
     def remove_redundancies(self):
         # combines any points with the same position
-        # this seems to have a high complexity, which may cause problems
         repeatedPoints = list()
         seenPoints = list()
         for v in self.vertices:
             neverSeen = True
             for q in seenPoints:
-                if np.allclose(v.position, q.position):
+                if np.allclose(self.vertices[v][1], self.vertices[q][1]):
                     repeatedPoints.append(v)
                     neverSeen = False
                     break
@@ -88,73 +65,66 @@ class Graph:
                 seenPoints.append(v)
         for p in repeatedPoints:
             for u in seenPoints:
-                if np.allclose(p.position, u.position):
+                if np.allclose(self.vertices[p][1], self.vertices[u][1]):
                     self.combine_vertices(p, u)
                     break
 
     def apply_harmonic_function(self):
         for v in self.vertices:
-            v.harmonicValue = v.position[0]  # starts with the function f(x, y) = x
+            self.vertices[v][2] = self.vertices[v][1][0]  # starts with the function f(x, y) = x
         greatestDifference = 1
         desiredAccuracy = .0000001
         while greatestDifference > desiredAccuracy:
             greatestDifference = 0
             for u in self.vertices:
-                if not (u.harmonicValue == 0 or u.harmonicValue == 1):
-                    oldHarmonicValue = copy.deepcopy(u.harmonicValue)
+                if not (self.vertices[u][2] == 0 or self.vertices[u][2] == 1):
+                    oldHarmonicValue = int(self.vertices[u][2])
                     listOfHarmonicValues = []
-                    for n in u.neighbors:
-                        listOfHarmonicValues.append(n.harmonicValue)
-                    u.harmonicValue = sum(listOfHarmonicValues) / len(listOfHarmonicValues)
-                    differenceBetweenHarmonicValues = abs(oldHarmonicValue - u.harmonicValue)
+                    for n in self.vertices[u][0]:
+                        listOfHarmonicValues.append(self.vertices[n][2])
+                    self.vertices[u][2] = np.mean(listOfHarmonicValues)
+                    differenceBetweenHarmonicValues = abs(oldHarmonicValue - self.vertices[u][2])
                     if differenceBetweenHarmonicValues > greatestDifference:
                         greatestDifference = differenceBetweenHarmonicValues
 
     def resistance_of_graph(self):
         totalOfSquaredDifferences = 0
         for v in self.vertices:
-            for n in v.neighbors:
-                totalOfSquaredDifferences += (v.harmonicValue - n.harmonicValue)**2
+            for n in self.vertices[v][0]:
+                totalOfSquaredDifferences += (self.vertices[v][2] - self.vertices[n][2]) ** 2
         return 1 / (totalOfSquaredDifferences / 2)
 
     def print_graph(self):
-        for i in range(len(self.vertices)):
-            print(self.vertices[i].name + " has neighbors:")
-            for n in self.vertices[i].neighbors:
-                print(n.name)
+        for v in self.vertices:
+            print(v, "has neighbors:")
+            for n in self.vertices[v][0]:
+                print(n)
             print("and is in position:")
-            print(self.vertices[i].position)
+            print(self.vertices[v][1])
             print("and has a harmonic function value of:")
-            print(self.vertices[i].harmonicValue)
+            print(self.vertices[v][2])
             print()
 
     def print_vertices_x_y_f(self):
-        for i in range(len(self.vertices)):
-            print(self.vertices[i].name, self.vertices[i].position, self.vertices[i].harmonicValue)
+        for v in self.vertices:
+            print(v, self.vertices[v][1], self.vertices[v][2])
 
 
 # test code
-"""
-g = Graph()
+'''g = Graph()
 f = Graph()
-a = Vertex("a1")
-b = Vertex("b1")
-c = Vertex("c1")
-d = Vertex("d1")
-a.position = np.array([0, 0])
-b.position = np.array([1, 0])
-c.position = np.array([0, 1])
-d.position = np.array([0, 0])
-# a and d should be combined
-g.add_vertex(a)
-g.add_vertex(b)
-g.add_vertex(c)
-g.add_vertex(d)
-g.add_edge(a, b)
-g.add_edge(c, d)
+g.add_vertex("a", [0, 0])
+g.add_vertex("b", [1, 0])
+g.add_vertex("c", [1, 1])
+g.add_vertex("d", [1, 0])
+f.add_vertex("e", [5, 5])
+f.add_vertex("h", [2, 2])
+g.add_edge("a", "b")
+g.add_edge("c", "d")
+f.add_edge("e", "h")
+g.add_graph(f)
+g.update_vertex_name("b", "t")
 print("graph 1")
-g.print_graph()
 g.remove_redundancies()
-print("graph 2")
 g.print_graph()
-"""
+g.print_vertices_x_y_f()'''
--- sierpinskiCarpet.py
+++ sierpinskiCarpet.py
@@ -3,32 +3,24 @@ import graphClasses as gc
 import copy
 
 #  INPUT HERE what level precarpet would you like?
-precarpet_level = 2
+precarpet_level = 0
 
 # building the level 0 cross carpet
 sC0 = gc.Graph()
-a = gc.Vertex("a")
-b = gc.Vertex("b")
-c = gc.Vertex("c")
-d = gc.Vertex("d")
-e = gc.Vertex("e")
-a.position = np.array([0, 0.5])
-b.position = np.array([0.5, 1])
-c.position = np.array([1, .5])
-d.position = np.array([0.5, 0])
-e.position = np.array([0.5, 0.5])
-sC0.add_vertex(a)
-sC0.add_vertex(b)
-sC0.add_vertex(c)
-sC0.add_vertex(d)
-sC0.add_vertex(e)
-sC0.add_edge(a, e)
-sC0.add_edge(b, e)
-sC0.add_edge(c, e)
-sC0.add_edge(d, e)
+sC0.add_vertex("a", np.array([0, 0.5]))
+sC0.add_vertex("b", np.array([0.5, 1]))
+sC0.add_vertex("c", np.array([1, .5]))
+sC0.add_vertex("d", np.array([0.5, 0]))
+sC0.add_vertex("e", np.array([0.5, 0.5]))
+sC0.add_edge("a", "e")
+sC0.add_edge('b', 'e')
+sC0.add_edge('c', 'e')
+sC0.add_edge('d', 'e')
 
-sCn = copy.deepcopy(sC0)
+sCn = gc.Graph()
+sCn.add_graph(sC0)
 sCn_plus_one = gc.Graph()
+copyOfSCn = gc.Graph()
 scalingFactor = 1 / 3  # how much longer each graph connection is compared to its n+1 carpet counterpart
 listOfFixedPoints = [np.array([0, 0]),  # q0
                      np.array([0.5, 0]),  # q1
@@ -39,20 +31,23 @@ listOfFixedPoints = [np.array([0, 0]),  # q0
                      np.array([0, 1]),  # q6
                      np.array([0, 0.5])]  # q7
 
+if precarpet_level == 0:
+    # the loop wont run, so just have this here for the edge case
+    sCn_plus_one.add_graph(sC0)
+
 for k in range(precarpet_level):
     sCn_plus_one = gc.Graph()
     for i in range(0, 8):
         copyOfSCn = copy.deepcopy(sCn)
-        for j in range(len(copyOfSCn.vertices)):
-            copyOfSCn.vertices[j].name = copyOfSCn.vertices[j].name + str(i)
+        copyOfSCn.update_all_vertices_names(str(i))
         copyOfSCn.contract_graph(scalingFactor, listOfFixedPoints[i])
         sCn_plus_one.add_graph(copyOfSCn)
     sCn_plus_one.remove_redundancies()
-    sCn = copy.deepcopy(sCn_plus_one)
+    sCn = gc.Graph()
+    sCn.add_graph(sCn_plus_one)
 
 sCn_plus_one.apply_harmonic_function()
-for v in sCn_plus_one.vertices:
-    print(v.name)
+print(sCn_plus_one.vertices)
 sCn_plus_one.print_graph()
 sCn_plus_one.print_vertices_x_y_f()
 print("Resistance of the graph is", sCn_plus_one.resistance_of_graph())
